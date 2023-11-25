a17354f15b979048e139f0db195dc9a2146f0201
Adrian Jamrowski <adrian.jamrowski@interia.pl>
2023-11-25 10:50:52 +0100

    Pierwszy projekt

---
+++ components/top/Nav.vue
@@ -0,0 +1,83 @@
+<script setup>
+    const name = ref("NAWIGACJA");
+</script>
+
+
+<template>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://bulma.io">
+      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+    </a>
+
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item">
+        Home
+      </a>
+
+      <a class="navbar-item">
+        Documentation
+      </a>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          More
+        </a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            About
+          </a>
+          <a class="navbar-item">
+            Jobs
+          </a>
+          <a class="navbar-item">
+            Contact
+          </a>
+          <hr class="navbar-divider">
+          <a class="navbar-item">
+            Report an issue
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-light">
+            Log in
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+    <h1>{{ name }}</h1>
+    <ul>
+        <li><nuxt-link to="/">Strona g³ówna</nuxt-link></li>
+        <li><nuxt-link to="/about">O nas</nuxt-link></li>
+        <li><nuxt-link to="/articles/1">Artyku³ 1</nuxt-link></li>
+        <li><nuxt-link to="/articles/2">Artyku³ 2</nuxt-link></li>
+    </ul>
+</template>
+<style scoped lang="scss">
+
+    h1{
+        color: violet;
+    }
+
+
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h3>To jest strona g³ówna</h3>
+</template>
